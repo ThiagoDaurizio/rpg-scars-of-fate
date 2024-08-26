ac006f8735780aaf8426eb06d9880a1a5c0c3add
thiagodaurizio <thiago_daurizio@hotmail.com>
2024-08-26 20:56:43 -0300

    update

--- src/app/assets/icons.ts
+++ src/app/assets/icons.ts
@@ -1,6 +1,13 @@
 
 import { HiOutlineSelector as markerIcon } from "react-icons/hi";
-import { HiChevronLeft as arrowToLeft, HiChevronRight as arrowToRight } from "react-icons/hi2";
+import { HiChevronLeft as arrowToLeft, HiChevronRight as arrowToRight } from "react-icons/hi";
+
+import { IoShieldOutline as classWarrior, IoSkullOutline as classWilden } from "react-icons/io5";
+import { FaRegEye as classAcolyte } from "react-icons/fa";
+import { LuSunMedium as classCleric } from "react-icons/lu";
+import { RiBook2Line as classMage } from "react-icons/ri";
+import { LuLeaf as classDruid } from "react-icons/lu";
+
 
 
 export const genericIcons = {
@@ -8,15 +15,6 @@ export const genericIcons = {
   ArrowToRight: arrowToRight,
   MarkerIcon: markerIcon
 }
-
-
-
-import { IoShieldOutline as classWarrior, IoSkullOutline as classWilden } from "react-icons/io5";
-import { FaRegEye as classAcolyte } from "react-icons/fa6";
-import { LuSunMedium as classCleric } from "react-icons/lu";
-import { RiBook2Line as classMage } from "react-icons/ri";
-import { LuLeaf as classDruid } from "react-icons/lu";
-
 
 export const classesIcons = {
   Warrior: classWarrior,
