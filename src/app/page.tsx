a3098f539cdb588fe402b5a6002bbf1fbaf75534
thiagodaurizio <thiago_daurizio@hotmail.com>
2025-02-24 23:54:02 -0300

    feat: druid3 update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex max-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col gap-4">
+        <p className="text-lg">Projeto ainda em contrução, veja as classes.</p>
+        <a href="/classes" className="bg-slate-800 p-8 py-4 text-center text-2xl rounded-md hover:bg-slate-800/85 hover:text-slate-100 font-bold cursor-pointer border border-white">CLASSES</a>
+      </div>
     </main>
   );
 }
